ccca2ec9fc9efbea35497e584410130a5520eca9
Alex <alex@alexs-imac.local>
2016-01-21 00:57:04 +0200

    Initial Commit

---
+++ import_audio_file/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  import_audio_file
+//
+//  Created by Alex on 21.01.16.
+//  Copyright © 2016 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
